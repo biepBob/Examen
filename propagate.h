c6e0601b409f20492f658625f443d598411dca0e
ProgvFysTycho <39005972+ProgvFysTycho@users.noreply.github.com>
2018-05-22 04:24:07 +0200

    Added comments

--- propagate.h
+++ propagate.h
@@ -23,52 +23,52 @@ class propagate{
 		propagate(const string fileName, float E, int Size,  const string EvalImageString, const string EvalLabelString, const string TrainImageString, const string TrainLabelString); // constructor 2
 		~propagate(); // destuctor
 
-		void setEta(float);		// Set the value of the learning rate 'Eta'
-		void setMiniBatchSize(int);
-		void setThreshold(float);
+		void setEta(float);		// Sets the value of the learning rate
+		void setMiniBatchSize(int);	// Sets the number of training samples in a minibatch
+		void setThreshold(float);	// Sets the threshold value for the outputs of the last layer
 		
-		void setTrainImages(batch&);
-		void setEvalImages(batch&);
+		void setTrainImages(batch&);	// Sets the vector containing the images for training the network
+		void setEvalImages(batch&);	// Sets the vector containing the images for evaluating (testing) the network
 		
-		float getEta() const;
-		int getMiniBatchSize() const;
-		float getThreshold() const;
+		float getEta() const;		// Gets the value of the learning rate
+		int getMiniBatchSize() const;	// Gets the number of training samples in a minibatch
+		float getThreshold() const;	// Gets the threshold value for the outputs of the last layer
 		
-		void loadNetwork(const string );
-		void saveNetwork(const string );
+		void loadNetwork(const string );	// Loads the biases and weights from a file to the current network
+		void saveNetwork(const string );	// Saves the current biases and weights of the network to a file 
 		
-		vector<float> resultFunc(const vector<float>& Input);
-		tuple<vector<int>,vector<vector<float>>> evaluateHistogram();
+		vector<float> resultFunc(const vector<float>& Input);	// Calculates the ouput of the last layer -> output of the network
+		tuple<vector<int>,vector<vector<float>>> evaluateHistogram();	// Gives values for showing the ouput of the network with the number it should represent
 
-		vector<vector<float>> propagateNEpochs(const int N);
-		vector<float> evaluate();
+		vector<vector<float>> propagateNEpochs(const int N);	// Trains the network for 'N' epochs
+		vector<float> evaluate();				// Calculates the values to evaluate the performing of the network
 
 
 	protected:
 	
-		vector<float> IntToVec(const float);
+		vector<float> IntToVec(const float);	// Returns a vector that respresents one given number between 0-9
 		
-		float propagateEpoch();
-		void propagateMiniBatch();
+		float propagateEpoch();		// Trains the network for one epoch
+		void propagateMiniBatch();	// Trains the network for 'MiniBatchSize' training samples and then derives the weights and biases
 		void deriveWeightsAndBias(const vector<vector<float>>&, const vector<vector<vector<float>>>&);	// Adjust the biases and weights of the network by a gradient descend method
 	
 		// Data members
 		float Eta;		// Learning rate 'eta'
-		int MiniBatchSize;	// Number of training sets in a minibatch
+		int MiniBatchSize;	// Number of training samples in a minibatch
 		
 		float CostMiniBatch;	// Mean costfunction of one minibatch
-		float TrainingError; // training error of the epoch
-		float ValidationError; // Validation error of the epoch
-		float Threshold = 0.5;
+		float TrainingError;	// Training error of one epoch
+		float ValidationError;	// Validation error of one epoch
+		float Threshold = 0.5;	// Threshold value for the outputs of the last layer
 		
-		vector<int> ChooseTrainingSample;
+		vector<int> ChooseTrainingSample;	// Vector that is used to select a random training sample
 		
 		network CurrentNetwork;	// The network that is being trained
 		
-		batch EvaluationBatch;
-		batch TrainingBatch;
-		vector<shared_ptr<testimage>> TrainImages;	// datamember -> Only need to get the images once
-		vector<shared_ptr<testimage>> EvalImages;	// datamember -> Only need to get the images once
+		batch TrainingBatch;		// Contains the training images of the MINST database
+		batch EvaluationBatch;		// Contains the evaluation (test) images of the MINST database
+		vector<shared_ptr<testimage>> TrainImages;	// Contains the pixel values and number it represents of each training image
+		vector<shared_ptr<testimage>> EvalImages;	// Contains the pixel values and number it represents of each evaluate (test) image
 		
 	private:
 		propagate(const propagate& prop);		// copyconstructor
